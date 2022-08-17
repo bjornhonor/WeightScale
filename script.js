17f28ce4646454def233aeb28d44cf6b3bf48525
bjorn <brunocarrarabpc@gmail.com>
2022-08-17 19:10:17 -0300

    ooo

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const balanca2 = {
     alimento: '',
     status: 'Vazia'
 }
+
+const batata = {
+    nome: "Batata",
+    pesoDeTroca: 100,
+}
+
+
+
+var combo1 = document.getElementById("c1");
+var combo2 = document.getElementById("c2");
+console.log(combo1);
